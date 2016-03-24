cffd4ebde4712457272547d1e7dcd20fc6a3bfdd
藤田一穂 <hujita.k@5sg.jp>
2016-03-24 16:02:21 +0900

    FIXME：ブロック連結しても連結判定が走らない。連結済みの状態でもう一度クリックしてブロックを掴むと判定が走る。

---
+++ puzzle_03/Assets/Scripts/BlockControl.cs
@@ -0,0 +1,384 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Infomation of block
+public class Block {
+	// Size of block collision
+	public static float COLLISION_SIZE = 0.9f;
+	public static float COLLISION_SPACE = 0.2f;
+	// Time to Vanish
+	public static float VANISH_TIME = 3.0f;
+	// Grid
+	public struct iPosition {
+		public int x;
+		public int y;
+	}
+	
+	public enum COLOR {
+		NONE = -1,
+		PINK = 0,
+		BLUE,
+		YELLOW,
+		GRAY,
+		MAGENTA,
+		WHITE,
+		NUM,
+		FIRST = PINK,
+		LAST = WHITE,
+		NORMAL_COLOR_NUM = NUM,
+	}
+
+	public enum DIR4 {
+		NONE = -1,
+		RIGHT,
+		LEFT,
+		UP,
+		DOWN,
+		NUM,
+	};
+
+	public enum STEP {
+		NONE = -1,
+		IDLE = 0,
+		GRABBED,
+		RELEASED,
+		SLIDE,
+		VACANT,
+		RESPAWN,
+		FALL,
+		LONG_SLIDE,
+		NUM,
+	};
+
+	public static int BLOCK_NUM_X = 6;
+	public static int BLOCK_NUM_Y = 7;
+}
+
+public class BlockControl : MonoBehaviour {
+	public Block.COLOR color = (Block.COLOR)0;
+	public BlockRoot block_root = null;
+	public Block.iPosition i_pos;
+	
+	public Block.STEP step = Block.STEP.NONE;
+	public Block.STEP next_step = Block.STEP.NONE;
+	private Vector3 position_offset_initial = Vector3.zero;
+	public Vector3 position_offset = Vector3.zero;
+	
+	public float vanish_timer = -1.0f;
+	public Block.DIR4 slide_dir = Block.DIR4.NONE;
+	public float step_timer = 0.0f;
+	
+	public Material opaque_material;
+	public Material transparent_material;
+	
+	private struct StepFall {
+		public float velocity;
+	}
+	private StepFall fall;
+	
+	// Use this for initialization
+	void Start () {
+		this.setColor (this.color);
+		this.next_step = Block.STEP.IDLE;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mouse_position;
+		this.block_root.unprojectMousePosition (out mouse_position, Input.mousePosition);
+		Vector2 mouse_position_xy = new Vector2 (mouse_position.x, mouse_position.y);
+		
+		if (this.vanish_timer >= 0.0f) {
+			this.vanish_timer -= Time.deltaTime;
+			if (this.vanish_timer < 0.0f) {
+				if (this.step != Block.STEP.SLIDE) {
+					this.vanish_timer = -1.0f;
+					this.next_step = Block.STEP.VACANT;
+				} else {
+					this.vanish_timer = 0.0f;
+				}
+			}
+		}
+		
+		this.step_timer += Time.deltaTime;
+		float slide_time = 0.2f;
+		
+		if (this.next_step == Block.STEP.NONE) {
+			switch (this.step) {
+			case Block.STEP.SLIDE:
+				if (this.step_timer >= slide_time) {
+					if (this.vanish_timer == 0.0f) {
+						this.next_step = Block.STEP.VACANT;
+					} else {
+						this.next_step = Block.STEP.IDLE;
+					}
+				}
+				break;
+			case Block.STEP.IDLE:
+				this.GetComponent<Renderer>().enabled = true;
+				break;
+			case Block.STEP.FALL:
+				if(this.position_offset.y <= 0.0f) {
+					this.next_step = Block.STEP.IDLE;
+					this.position_offset.y = 0.0f;
+				}
+				break;
+			}
+		}
+		
+		while (this.next_step != Block.STEP.NONE) {
+			this.step = this.next_step;
+			this.next_step = Block.STEP.NONE;
+			
+			switch (this.step) {
+			case Block.STEP.IDLE:
+				this.position_offset = Vector3.zero;
+				this.transform.localScale = Vector3.one * 1.0f;
+				break;
+			case Block.STEP.GRABBED:
+				this.transform.localScale = Vector3.one * 1.2f;
+				break;
+			case Block.STEP.RELEASED:
+				this.position_offset = Vector3.zero;
+				this.transform.localScale = Vector3.one * 1.0f;
+				break;
+			case Block.STEP.VACANT:
+				this.position_offset = Vector3.zero;
+				this.setVisible(false);
+				break;
+			case Block.STEP.RESPAWN:
+				int color_index = Random.Range (0, (int)Block.COLOR.NORMAL_COLOR_NUM);
+				this.setColor((Block.COLOR)color_index);
+				this.next_step = Block.STEP.IDLE;
+				break;
+				
+			case Block.STEP.FALL:
+				this.setVisible(true);
+				this.fall.velocity = 0.0f;
+				break;
+			}
+			this.step_timer = 0.0f;
+		}
+		
+		switch(this.step) {
+		case Block.STEP.GRABBED:
+			this.slide_dir = this.calcSlideDir (mouse_position_xy);
+			break;
+		case Block.STEP.SLIDE:
+			float rate = this.step_timer / slide_time;
+			rate = Mathf.Min (rate, 1.0f);
+			rate = Mathf.Sin (rate*Mathf.PI / 2.0f);
+			this.position_offset = Vector3.Lerp (this.position_offset_initial, Vector3.zero, rate);
+			break;
+		case Block.STEP.FALL:
+			this.fall.velocity += Physics.gravity.y * Time.deltaTime * 0.3f;
+			this.position_offset.y += this.fall.velocity * Time.deltaTime;
+			if(this.position_offset.y < 0.0f) {
+				this.position_offset.y = 0.0f;
+			}
+			break;
+		}
+		Vector3 position = BlockRoot.calcBlockPosition(this.i_pos) + this.position_offset;
+		
+		this.transform.position = position;
+		
+		this.setColor (this.color);
+		if (this.vanish_timer >= 0.0f) {
+			Color color0 = Color.Lerp (this.GetComponent<Renderer>().material.color, Color.white, 0.5f);
+			Color color1 = Color.Lerp (this.GetComponent<Renderer>().material.color, Color.black, 0.5f);
+			
+			if (this.vanish_timer < Block.VANISH_TIME / 2.0f) {
+				color0.a = this.vanish_timer / (Block.VANISH_TIME / 2.0f);
+				color1.a = color0.a;
+				
+				this.GetComponent<Renderer>().material = this.transparent_material;
+			}
+			float rate = 1.0f - this.vanish_timer / Block.VANISH_TIME;
+			this.GetComponent<Renderer>().material.color = Color.Lerp (color0, color1, rate);
+		}
+	}
+	
+	public void setColor(Block.COLOR color) {
+		this.color = color;
+		Color color_value;
+		
+		switch (this.color) {
+		default:
+		case Block.COLOR.PINK:
+			color_value = new Color(1.0f, 0.5f, 0.5f);
+			break;
+		case Block.COLOR.BLUE:
+			color_value = Color.blue;
+			break;
+		case Block.COLOR.YELLOW:
+			color_value = Color.yellow;
+			break;
+		case Block.COLOR.GRAY:
+			color_value = Color.gray;
+			break;
+		case Block.COLOR.MAGENTA:
+			color_value = Color.magenta;
+			break;
+		case Block.COLOR.WHITE:
+			color_value = Color.white;
+			break;
+		}
+		this.GetComponent<Renderer>().material.color = color_value;
+	}
+	
+	public void beginGrab(){
+		this.next_step = Block.STEP.GRABBED;
+	}
+	
+	public void endGrab() {
+		this.next_step = Block.STEP.IDLE;
+	}
+	
+	public bool isGrabbable() {
+		bool is_grabbable = false;
+		
+		switch (this.step) {
+		case Block.STEP.IDLE:
+			is_grabbable = true;
+			break;
+		}
+		return(is_grabbable);
+	}
+	
+	public bool isContainedPosition(Vector2 position) {
+		bool ret = false;
+		Vector3 center = this.transform.position;
+		float h = Block.COLLISION_SIZE / 2.0f;
+		
+		do {
+			if (position.x < center.x - h || center.x + h < position.x) {
+				break;
+			}
+			if (position.y < center.y - h || center.y + h < position.y) {
+				break;
+			}
+			
+			ret = true;
+		} while(false);
+		
+		return(ret);
+	}
+	
+	
+	public Block.DIR4 calcSlideDir(Vector2 mouse_position) {
+		Block.DIR4 dir = Block.DIR4.NONE;
+		Vector2 v = mouse_position - new Vector2 (this.transform.position.x, this.transform.position.y);
+		
+		if (v.magnitude > 0.1f) {
+			if(v.y > v.x) {
+				if (v.y > -v.x) {
+					dir = Block.DIR4.UP;
+				} else {
+					dir = Block.DIR4.LEFT;
+				}
+			} else {
+				if (v.y > -v.x) {
+					dir = Block.DIR4.RIGHT;
+				} else {
+					dir = Block.DIR4.DOWN;
+				}
+			}
+		}
+		return(dir);
+	}
+	
+	public float calcDirOffset(Vector2 position, Block.DIR4 dir)
+	{
+		float offset = 0.0f;
+		Vector2 v = position - new Vector2 (this.transform.position.x, this.transform.position.y);
+		switch (dir) {
+		case Block.DIR4.RIGHT:
+			offset = v.x;
+			break;
+		case Block.DIR4.LEFT:
+			offset = -v.x;
+			break;
+		case Block.DIR4.UP:
+			offset = v.y;
+			break;
+		case Block.DIR4.DOWN:
+			offset = -v.y;
+			break;
+		}
+		return(offset);
+	}
+	
+	public void beginSlide(Vector3 offset)
+	{
+		this.position_offset_initial = offset;
+		this.position_offset = this.position_offset_initial;
+		
+		this.next_step = Block.STEP.SLIDE;
+	}
+	
+	public void toVanishing()
+	{
+		this.vanish_timer = Block.VANISH_TIME;
+	}
+	
+	public bool isVanishing()
+	{
+		bool is_vanishing = (this.vanish_timer > 0.0f);
+		return(is_vanishing);
+	}
+	
+	public void rewindVanishTimer()
+	{
+		this.vanish_timer = Block.VANISH_TIME;
+	}
+	
+	public bool isVisible()
+	{
+		bool is_visible = this.GetComponent<Renderer>().enabled;
+		return(is_visible);
+	}
+	
+	public void setVisible(bool is_visible)
+	{
+		this.GetComponent<Renderer>().enabled = is_visible;
+	}
+	
+	public bool isIdle()
+	{
+		bool is_idle = false;
+		if (this.step == Block.STEP.IDLE && this.next_step == Block.STEP.NONE) {
+			is_idle = true;
+		}
+		return(is_idle);
+	}
+	
+	public void beginFall(BlockControl start)
+	{
+		this.next_step = Block.STEP.FALL;
+		this.position_offset.y = (float)(start.i_pos.y - this.i_pos.y) * Block.COLLISION_SIZE;
+	}
+	
+	public void beginRespawn(int start_i_pos_y)
+	{
+		this.position_offset.y = (float)(start_i_pos_y - this.i_pos.y) * Block.COLLISION_SIZE;
+		this.next_step = Block.STEP.FALL;
+		int color_index = Random.Range ((int)Block.COLOR.FIRST, (int)Block.COLOR.LAST + 1);
+		this.setColor ((Block.COLOR)color_index);
+	}
+	
+	public bool isVacant()
+	{
+		bool is_vacant = false;
+		if (this.step == Block.STEP.VACANT && this.next_step == Block.STEP.NONE) {
+			is_vacant = true;
+		}
+		return(is_vacant);
+	}
+	
+	public bool isSliding()
+	{
+		bool is_sliding = (this.position_offset.x != 0.0f);
+		return(is_sliding);
+	}
+	
+}
---
+++ puzzle_03/Assets/Scripts/BlockRoot.cs
@@ -0,0 +1,484 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockRoot : MonoBehaviour {
+	public GameObject BlockPrefab = null;
+	public BlockControl[,] blocks;
+	
+	private GameObject main_camera = null;
+	private BlockControl grabbed_block = null;
+
+	
+	// Use this for initialization
+	void Start () {
+		this.main_camera = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mouse_position;
+		this.unprojectMousePosition (out mouse_position, Input.mousePosition);
+		Vector2 mouse_position_xy = new Vector2 (mouse_position.x, mouse_position.y);
+		
+		if (this.grabbed_block == null) {
+			// if( && this.is_grabbable_all() == true){}
+			if (!this.is_has_falling_block ()) {
+				if (Input.GetMouseButtonDown (0)) {
+					foreach (BlockControl block in this.blocks) {
+						if (! block.isGrabbable ()) {
+							continue;
+						}
+						
+						if (!block.isContainedPosition (mouse_position_xy)) {
+							continue;
+						}
+						
+						this.grabbed_block = block;
+						this.grabbed_block.beginGrab ();
+						break;
+					}
+				}
+			}
+		} else {
+			do {
+				BlockControl swap_target = this.getNextBlock (grabbed_block, grabbed_block.slide_dir);
+				if (swap_target == null) {
+					break;
+				}
+				if (! swap_target.isGrabbable ()) {
+					break;
+				}
+				float offset = this.grabbed_block.calcDirOffset (mouse_position_xy, this.grabbed_block.slide_dir);
+				if (offset < Block.COLLISION_SIZE / 2.0f) {
+					break;
+				}
+				this.swapBlock (grabbed_block, grabbed_block.slide_dir, swap_target);
+				this.grabbed_block = null;
+			} while(false);
+			
+			if (! Input.GetMouseButton (0)) {
+				this.grabbed_block.endGrab ();
+				this.grabbed_block = null;
+			}
+		}
+			
+		if (this.is_has_falling_block () || this.is_has_sliding_block ()) {
+		} else {
+			int ignite_count = 0;
+			foreach (BlockControl  block in this.blocks) {
+				if (! block.isIdle ()) {
+					continue;
+				}
+				if (this.checkConnection (block)) {
+					ignite_count++;
+				}
+			}
+				
+			if (ignite_count > 0) {
+				int block_count = 0;
+				foreach (BlockControl block in this.blocks) {
+					if (block.isVanishing ()) {
+						//block.rewindVanishTimer ();
+					}
+				}
+			}
+		}
+		
+		bool is_vanishing = this.is_has_vanishing_block ();
+		do {
+			if (is_vanishing) {
+				break;
+			}
+			if (this.is_has_sliding_block ()) {
+				break;
+			}
+			for (int x = 0; x < Block.BLOCK_NUM_X; x++) {
+				if (this.is_has_sliding_block_in_column (x)) {
+					continue;
+				}
+				for (int y = 0; y < Block.BLOCK_NUM_Y - 1; y++) {
+					if (! this.blocks [x, y].isVacant ()) {
+						continue;
+					}
+					for (int y1 = y + 1; y1 < Block.BLOCK_NUM_Y; y1++) {
+						if (this.blocks [x, y1].isVacant ()) {
+							continue;
+						}
+						this.fallBlock (this.blocks [x, y], Block.DIR4.UP,
+						                this.blocks [x, y1]);
+						break;
+					}
+				}
+			}
+			
+			for (int x = 0; x < Block.BLOCK_NUM_X; x++) {
+				int fall_start_y = Block.BLOCK_NUM_Y;
+				for (int y = 0; y < Block.BLOCK_NUM_Y; y++) {
+					if (! this.blocks [x, y].isVacant ()) {
+						continue;
+					}
+					this.blocks [x, y].beginRespawn (fall_start_y);
+					fall_start_y++;
+				}
+			}
+		} while(false);
+	}
+	
+	// Creating blocks and deploy
+	public void initialSetUp() {
+		this.blocks = new BlockControl [Block.BLOCK_NUM_X, Block.BLOCK_NUM_Y];
+		
+		int color_index = 0;
+		
+		for (int y = 0; y < Block.BLOCK_NUM_Y; y++) {
+			for (int x = 0; x < Block.BLOCK_NUM_X; x++) {
+				GameObject game_object = Instantiate (this.BlockPrefab) as GameObject;
+				BlockControl block = game_object.GetComponent<BlockControl>();
+				this.blocks[x, y] = block;
+				
+				block.i_pos.x = x;
+				block.i_pos.y = y;
+				
+				block.block_root = this;
+				
+				Vector3 position = BlockRoot.calcBlockPosition(block.i_pos);
+				block.transform.position = position;
+				block.setColor ((Block.COLOR)color_index);
+				
+				block.name = "block(" + block.i_pos.x.ToString () + "," + block.i_pos.y.ToString() + ")";
+				
+				color_index = Random.Range (0, (int)Block.COLOR.NORMAL_COLOR_NUM);
+				
+			}
+		}
+	}
+	
+	public static Vector3 calcBlockPosition(Block.iPosition i_pos) {
+		Vector3 position = new Vector3 (-(Block.BLOCK_NUM_X / 2.0f - 0.4f),
+		                                -(Block.BLOCK_NUM_Y / 2.0f - 1.0f), 0.0f);
+		
+		position.x += (float)i_pos.x * Block.COLLISION_SIZE;
+		position.y += (float)i_pos.y * Block.COLLISION_SIZE;
+		
+		return(position);
+	}
+	
+	public bool unprojectMousePosition(out Vector3 world_position, Vector3 mouse_position) {
+		bool ret;
+		Plane plane = new Plane (Vector3.back, new Vector3 (0.0f, 0.0f, -Block.COLLISION_SIZE / 2.0f));
+		
+		Ray ray = this.main_camera.GetComponent<Camera> ().ScreenPointToRay (mouse_position);
+		float depth;
+		
+		if (plane.Raycast (ray, out depth)) {
+			world_position = ray.origin + ray.direction * depth;
+			ret = true;
+		} else {
+			world_position = Vector3.zero;
+			ret = false;
+		}
+		
+		return(ret);
+	}
+	
+	public BlockControl getNextBlock(BlockControl block, Block.DIR4 dir)
+	{
+		BlockControl next_block = null;
+		
+		switch (dir) {
+		case Block.DIR4.RIGHT:
+			if (block.i_pos.x < Block.BLOCK_NUM_X - 1) {
+				next_block = this.blocks[block.i_pos.x + 1, block.i_pos.y];
+			}
+			break;
+		case Block.DIR4.LEFT:
+			if (block.i_pos.x > 0) {
+				next_block = this.blocks[block.i_pos.x - 1, block.i_pos.y];
+			}
+			break;
+		case Block.DIR4.UP:
+			if (block.i_pos.y < Block.BLOCK_NUM_Y - 1) {
+				next_block = this.blocks[block.i_pos.x, block.i_pos.y + 1];
+			}
+			break;
+		case Block.DIR4.DOWN:
+			if (block.i_pos.y > 0) {
+				next_block = this.blocks[block.i_pos.x, block.i_pos.y - 1];
+			}
+			break;
+		}
+		return(next_block);
+	}
+	
+	public static Vector3 getDirVector(Block.DIR4 dir)
+	{
+		Vector3 v = Vector3.zero;
+		
+		switch (dir) {
+		case Block.DIR4.RIGHT:
+			v = Vector3.right;
+			break;
+		case Block.DIR4.LEFT:
+			v = Vector3.left;
+			break;
+		case Block.DIR4.UP:
+			v = Vector3.up;
+			break;
+		case Block.DIR4.DOWN:
+			v = Vector3.down;
+			break;
+		}
+		
+		v *= Block.COLLISION_SIZE;
+		return(v);
+	}
+	
+	public static Block.DIR4 getOppositDir(Block.DIR4 dir)
+	{
+		Block.DIR4 opposit = dir;
+		switch (dir) {
+		case Block.DIR4.RIGHT:
+			opposit = Block.DIR4.LEFT;
+			break;
+		case Block.DIR4.LEFT:
+			opposit = Block.DIR4.RIGHT;
+			break;
+		case Block.DIR4.UP:
+			opposit = Block.DIR4.DOWN;
+			break;
+		case Block.DIR4.DOWN:
+			opposit = Block.DIR4.UP;
+			break;
+		}
+		return(opposit);
+	}
+	
+	public void swapBlock(BlockControl block0, Block.DIR4 dir, BlockControl block1)
+	{
+		Block.COLOR color0 = block0.color;
+		Block.COLOR color1 = block1.color;
+		
+		Vector3 scale0 = block0.transform.localScale;
+		Vector3 scale1 = block1.transform.localScale;
+		
+		float vanish_timer0 = block0.vanish_timer;
+		float vanish_timer1 = block1.vanish_timer;
+		
+		Vector3 offset0 = BlockRoot.getDirVector (dir);
+		Vector3 offset1 = BlockRoot.getDirVector (BlockRoot.getOppositDir (dir));
+		
+		block0.setColor (color1);
+		block1.setColor (color0);
+		
+		block0.transform.localScale = scale1;
+		block1.transform.localScale = scale0;
+		
+		block0.vanish_timer = vanish_timer1;
+		block1.vanish_timer = vanish_timer0;
+		
+		block0.beginSlide (offset0);
+		block1.beginSlide (offset1);
+	}
+	
+	public bool checkConnection(BlockControl start)
+	{
+		bool ret = false;
+		int normal_block_num = 0;
+		
+		if (! start.isVanishing ()) {
+			normal_block_num = 1;
+		}
+		
+		int rx = start.i_pos.x;
+		int lx = start.i_pos.x;
+		
+		for (int x = lx - 1; x > 0; x--) {
+			BlockControl next_block = this.blocks [x, start.i_pos.y];
+			if (next_block.color != start.color) {
+				break;
+			}
+			if (next_block.step == Block.STEP.FALL || next_block.next_step == Block.STEP.FALL) {
+				break;
+			}
+			if (next_block.step == Block.STEP.SLIDE || next_block.next_step == Block.STEP.SLIDE) {
+				break;
+			}
+			if (! next_block.isVanishing ()) {
+				normal_block_num++;
+			}
+			lx = x;
+		}
+		
+		for (int x = rx + 1; x < Block.BLOCK_NUM_X; x++) {
+			BlockControl next_block = this.blocks [x, start.i_pos.y];
+			if (next_block.color != start.color) {
+				break;
+			}
+			if (next_block.step == Block.STEP.FALL || next_block.next_step == Block.STEP.FALL) {
+				break;
+			}
+			if (next_block.step == Block.STEP.SLIDE || next_block.next_step == Block.STEP.SLIDE) {
+				break;
+			}
+			if (! next_block.isVanishing ()) {
+				normal_block_num++;
+			}
+			rx = x;
+		}
+		
+		do {
+			if (rx - lx + 1 < 3) {
+				break;
+			}
+			if (normal_block_num == 0) {
+				break;
+			}
+			for (int x = lx; x < rx + 1; x++) {
+				this.blocks [x, start.i_pos.y].toVanishing ();
+				ret = true;
+			}
+		} while(false);
+		
+		normal_block_num = 0;
+		if (! start.isVanishing ()) {
+			normal_block_num = 1;
+		}
+		int uy = start.i_pos.y;
+		int dy = start.i_pos.y;
+		
+		for (int y = dy - 1; y > 0; y--) {
+			BlockControl next_block = this.blocks [start.i_pos.x, y];
+			if (next_block.color != start.color) {
+				break;
+			}
+			
+			if (next_block.step == Block.STEP.FALL || next_block.next_step == Block.STEP.FALL) {
+				break;
+			}
+			if (next_block.step == Block.STEP.SLIDE || next_block.next_step == Block.STEP.SLIDE) {
+				break;
+			}
+			if (! next_block.isVanishing ()) {
+				normal_block_num++;
+			}
+			dy = y;
+		}
+		
+		for (int y = uy + 1; y > Block.BLOCK_NUM_Y; y++) {
+			BlockControl next_block = this.blocks [start.i_pos.x, y];
+			if (next_block.color != start.color) {
+				break;
+			}
+			if (next_block.step == Block.STEP.FALL || next_block.next_step == Block.STEP.FALL) {
+				break;
+			}
+			if (next_block.step == Block.STEP.SLIDE || next_block.next_step == Block.STEP.SLIDE) {
+				break;
+			}
+			if (! next_block.isVanishing ()) {
+				normal_block_num++;
+			}
+			uy = y;
+		}
+		
+		do {
+			if (uy - dy + 1 < 3) {
+				break;
+			}
+			if (normal_block_num == 0) {
+				break;
+			}
+			for (int y = dy; y < uy + 1; y++) {
+				this.blocks [start.i_pos.x, y].toVanishing ();
+				ret = true;
+			}
+		} while(false);
+		return(ret);
+	}
+	
+	private bool is_has_vanishing_block()
+	{
+		bool ret = false;
+		foreach (BlockControl block in this.blocks) {
+			if (block.vanish_timer > 0.0f) {
+				ret = true;
+				break;
+			}
+		}
+		return(ret);
+	}
+	
+	private bool is_has_sliding_block()
+	{
+		bool ret = false;
+		foreach (BlockControl block in this.blocks) {
+			if (block.step == Block.STEP.SLIDE) {
+				ret = true;
+				break;
+			}
+		}
+		return(ret);
+	}
+	
+	private bool is_has_falling_block()
+	{
+		bool ret = false;
+		foreach (BlockControl block in this.blocks) {
+			if (block.step == Block.STEP.FALL) {
+				ret = true;
+				break;
+			}
+		}
+		return(ret);
+	}
+	
+	public void fallBlock(BlockControl block0, Block.DIR4 dir, BlockControl block1)
+	{
+		Block.COLOR color0 = block0.color;
+		Block.COLOR color1 = block1.color;
+		Vector3 scale0 = block0.transform.localScale;
+		Vector3 scale1 = block1.transform.localScale;
+		float vanish_timer0 = block0.vanish_timer;
+		float vanish_timer1 = block1.vanish_timer;
+		bool visible0 = block0.isVisible ();
+		bool visible1 = block1.isVisible ();
+		Block.STEP step0 = block0.step;
+		Block.STEP step1 = block1.step;
+		
+		block0.setColor (color1);
+		block1.setColor (color0);
+		block0.transform.localScale = scale1;
+		block1.transform.localScale = scale0;
+		block0.vanish_timer = vanish_timer1;
+		block1.vanish_timer = vanish_timer0;
+		block0.setVisible (visible1);
+		block1.setVisible (visible0);
+		block0.step = step1;
+		block1.step = step0;
+		block0.beginFall (block1);
+	}
+	
+	private bool is_has_sliding_block_in_column(int x)
+	{
+		bool ret = false;
+		for (int y = 0; y < Block.BLOCK_NUM_Y; y++) {
+			if (this.blocks [x, y].isSliding ()) {
+				ret = true;
+				break;
+			}
+		}
+		return(ret);
+	}
+
+	private bool is_grabbable_all() {
+		bool ret = true;
+		foreach (BlockControl block in this.blocks) {
+			if (block.isGrabbable() == false) {
+				ret = false;
+				break;
+			}
+		}
+		return(ret);
+	}
+	
+}
